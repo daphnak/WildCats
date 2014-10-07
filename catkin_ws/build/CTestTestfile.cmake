b708a73b4f541a39176a2e0f75bbc8ce4764190f
WildCat <khend@uw.edu>
2014-10-07 10:32:52 -0700

    moved all thebeginner tutorials to repo

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/motionlab/catkin_ws/src
+# Build directory: /home/motionlab/catkin_ws/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(beginner_tutorials)
---
+++ catkin_ws/src/beginner_tutorials/scripts/roomba.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from kobuki_msgs.msg import BumperEvent
+
+UNPROCESSED_BUMP = False
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id()+"I heard %s",data)
+    global UNPROCESSED_BUMP
+    UNPROCESSED_BUMP = True
+    
+
+def roomba():
+    pub = rospy.Publisher('mobile_base/commands/velocity', Twist)
+    rospy.init_node('roomber', anonymous=True)
+    rospy.Subscriber('/mobile_base/events/bumper', BumperEvent, callback)
+    twist = Twist()
+    r = rospy.Rate(3)
+    while not rospy.is_shutdown():
+        global UNPROCESSED_BUMP
+        if UNPROCESSED_BUMP:
+            UNPROCESSED_BUMP = False
+            twist.linear.x = -2
+            twist.angular.z = 1.5
+        else:
+            twist.linear.x = 1
+            twise.angular.z = 0.0
+        pub.publish(twist)
+        "rospy.loginfo(twist)"
+        r.sleep()
+        
+if __name__ == '__main__':
+    try:
+        roomba()
+    except rospy.ROSInterruptException: pass
+
+
+#  * /mobile_base/events/bumper [kobuki_msgs/BumperEvent]
