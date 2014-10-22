49a320eb1e0d60a65eaada57bf7a2b2ca8d4c58e
WildCat <khend@uw.edu>
2014-10-22 15:20:55 -0700

    some send work

---
+++ catkin_ws/src/beginner_tutorials/scripts/sendToLocation.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+import rospy
+import math
+from std_msgs.msg import String, Header
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
+from move_base_msgs.msg import MoveBaseActionGoal, MoveBaseGoal
+
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+
+PUB_GOAL = None
+PUBD = False
+
+def callback(data):
+    sendToOnce()
+
+def sendToOnce():
+    global PUB_GOAL, PUBD
+    # if PUBD:
+    #     return
+
+    # PUBD = True
+
+    DISTANCE_THRESHOLD = .1
+    print'********'
+    # x = data.pose.pose.position.x
+    # y = data.pose.pose.position.y
+    
+    # if math.sqrt(x**2+y**2) < DISTANCE_THRESHOLD:
+    #     return
+
+    ps = PoseStamped()
+    ps.header.frame_id = "/map"
+    ps.header.stamp = rospy.Time.now()
+    ps.pose.position.x = 1
+    ps.pose.position.y = 0
+    ps.pose.position.z = 0
+
+    q = quaternion_from_euler(0, 0, 0, 'sxyz')
+    ps.pose.orientation.x = q[0]
+    ps.pose.orientation.y = q[1]
+    ps.pose.orientation.z = q[2]
+    ps.pose.orientation.w = q[3]
+
+    mbag = MoveBaseActionGoal()
+    mbag.header = ps.header
+    mbag.goal.target_pose = ps
+
+
+    print mbag
+    PUB_GOAL.publish(mbag)
+
+
+
+'''
+def __init__(self):
+self.msg = PoseStamped()
+self.msg.header.frame_id = "/map"
+self.pub_ = rospy.Publisher('/move_base_simple/goal', PoseStamped)
+rospy.Subscriber('/recognizer/output', String, self.speechCb)
+rospy.spin()
+
+ def setMsg(self, goal):
+self.msg.pose.position.x = goals[goal][0]
+self.msg.pose.position.y = goals[goal][1]
+self.msg.pose.position.z = goals[goal][2]
+q = quaternion_from_euler(0, 0, goals[goal][3], 'sxyz')
+self.msg.pose.orientation.x = q[0]
+self.msg.pose.orientation.y = q[1]
+self.msg.pose.orientation.z = q[2]
+self.msg.pose.orientation.w = q[3]
+
+
+def speechCb(self, msg):
+rospy.loginfo(msg.data)
+self.msg.header.stamp = rospy.Time.now()
+if msg.data.find("lab") > -1:
+self.setMsg("The Lab")
+elif msg.data.find("hallway") > -1:
+self.setMsg("The Hallway")
+elif msg.data.find("ken") > -1:
+self.setMsg("Ken's Desk")
+elif msg.data.find("nick") > -1:
+self.setMsg("Nick's Office")
+else:
+rospy.loginfo("unknown goal!")
+return
+self.pub_.publish(self.msg)
+'''
+    
+     
+def wildcat():
+    rospy.init_node('wildcat_goes_home', anonymous=True)
+    
+    
+
+    global PUB_GOAL
+    # PUB_GOAL = rospy.Publisher('/move_base/goal', PoseStamped)
+    PUB_GOAL = rospy.Publisher('/move_base/goal', MoveBaseActionGoal)
+
+    # rospy.Subscriber('/odom', Odometry, callback)
+    for i in range(100):
+        sendToOnce()
+        # pass
+    
+    rospy.spin()
+
+def getDistance(point):
+    return math.sqrt(point[0]**2 + point[1]**2 + point[2]**2)
+
+        
+if __name__ == '__main__':
+    try:
+        wildcat()
+    except rospy.ROSInterruptException: pass
+ 
