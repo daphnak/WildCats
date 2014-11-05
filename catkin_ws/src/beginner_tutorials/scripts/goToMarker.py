6733dab1d214920910f6b717aba2c4b446bf2202
WildCat <khend@uw.edu>
2014-11-05 14:23:29 -0800

    who knows

--- catkin_ws/src/beginner_tutorials/scripts/goToMarker.py
+++ catkin_ws/src/beginner_tutorials/scripts/goToMarker.py
@@ -1,51 +1,56 @@
 #!/usr/bin/env python
 import rospy
+import threading
 import math
-from std_msgs.msg import String, Header
+from time import time
 from geometry_msgs.msg import Twist
-from geometry_msgs.msg import Point
-from geometry_msgs.msg import PointStamped
-from sensor_msgs.msg import PointCloud2, PointField
 from visualization_msgs.msg import Marker
-from gotoClient import goto
-PUB_MOTION = None
 
-def callback(response):
+
+PUB_MOTION = None
+PREV_TIME = 0
+
+# angular displacement direction needed
+# forward?
+
+def setInterval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+
+def markerHandler(response):
+        global PREV_TIME
         print'********'
+        now = time()
+        diff = now - PREV_TIME
+        print 'time since last callback' , diff
+        PREV_TIME = now
+
+
         pointPosition = response.pose.position
 
-
-            
-        # Create minimum distance point object for visualization purposes
-        # pointPosition = Point()
-        # pointPosition.x = minPoint[0]
-        # pointPosition.y = minPoint[1]
-        # pointPosition.z = minPoint[2]
-        # result = PointStamped()
-        # result.point = pointPosition
-        # header = Header()
-        # header.frame_id = 'camera_depth_optical_frame'
-        # result.header = header 
-        # PUB_MIN_POINT.publish(result)
-
-
-
-        # Move towards nearest point
         twist = Twist()
-        if pointPosition.x > 0.1:
-            twist.angular.z = -0.5
+        if pointPosition.x > 0.05:
+            twist.angular.z = -0.7
             print 1
-        elif pointPosition.x < -0.1:
-            twist.angular.z = 0.5
+        elif pointPosition.x < -0.05:
+            twist.angular.z = 0.7
             print 2
-        elif pointPosition.z > 0.2:
+        # elif pointPosition.z > 0.4:
+        if getDistance(pointPosition) > 0.4:
             twist.linear.x = 0.2 # or make negative to run away from things
             print 3
         PUB_MOTION.publish(twist)
 
+
+def getDistance(position):
+    return math.sqrt(position.y**2 + position.z**2)
+
 def wildcat():
     rospy.init_node('goToMarker', anonymous=True)
-    rospy.Subscriber('/visualization_marker', Marker, callback)
+    rospy.Subscriber('/wildcat_visualization_marker', Marker, markerHandler)
     global PUB_MOTION
     PUB_MOTION = rospy.Publisher('mobile_base/commands/velocity', Twist)
     rospy.spin()
