e6ce6ec1deb433389cc84af7454082e1135fddc3
WildCat <khend@uw.edu>
2014-11-11 14:45:50 -0800

    add image to woz html page

--- WildcatWebController/wildcat.js
+++ WildcatWebController/wildcat.js
@@ -6,24 +6,34 @@ window.onload = function() {
  * Setup all GUI elements when the page is loaded.
  */
 function init() {
-  // Connect to ROS.
-  var ros = new ROSLIB.Ros({
-    // url : 'ws://leatherback:9090'
-    url : 'http://leatherback.cs.washington.edu:9090'
+  // // Connect to ROS.
+  // var ros = new ROSLIB.Ros({
+  //   url : 'ws://leatherback:9090'
+  //   // url : 'http://leatherback.cs.washington.edu:9090'
+  // });
+
+  // // Create the nav navViewer.
+  // var navViewer = new ROS2D.navViewer({
+  //   divID : 'nav',
+  //   width : 544,
+  //   height : 512
+  // });
+
+  // // Setup the nav client.
+  // var nav = NAV2D.OccupancyGridClientNav({
+  //   ros : ros,
+  //   rootObject : navViewer.scene,
+  //   navViewer : navViewer,
+  //   serverName : '/move_base/goal'
+  // });
+
+  // Create the image viewer.
+  var imageViewer = new MJPEGCANVAS.Viewer({
+    divID : 'mjpeg',
+    host : 'localhost',
+    width : 640,
+    height : 480,
+    topic : '/camera/rgb/image_raw'
   });
 
-  // Create the main viewer.
-  var viewer = new ROS2D.Viewer({
-    divID : 'nav',
-    width : 544,
-    height : 512
-  });
-
-  // Setup the nav client.
-  var nav = NAV2D.OccupancyGridClientNav({
-    ros : ros,
-    rootObject : viewer.scene,
-    viewer : viewer,
-    serverName : '/move_base/goal'
-  });
 }
